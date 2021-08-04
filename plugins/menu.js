2b6639cb8704d0a9a0b3a367a5167d4fc067af9b
Megane <87269645+MisakaMikoto10@users.noreply.github.com>
2021-08-04 11:49:25 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -82,7 +82,7 @@ ${'```%npmdesc```'}
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
     let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-    let ciel = './src/photo/813490.jpg'
+    let ciel = './src/photo/PDIP.jpg'
     let unknownnzx = 'https://github.com/MisakaMikoto10'
     //let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
     let tnbot = (await conn.getFile(await conn.getProfilePicture(m.fromMe))).data.toString('base64')
@@ -180,7 +180,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     //conn.reply(m.chat, text.trim(), m)
-    await conn.sendFile(m.chat, ciel, '813490.jpeg', text.trim(), { 
+    await conn.sendFile(m.chat, ciel, 'PDIP.jpeg', text.trim(), { 
       key: { 
         remoteJid: 'status@broadcast', 
         participant: '0@s.whatsapp.net', 
